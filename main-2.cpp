f51b28cc8d6aaddc414ff582f59716edccae53c4
Mukul242426 <90404073+Mukul242426@users.noreply.github.com>
2021-09-09 20:43:48 +0530

    Add files via upload

---
+++ main-2.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void Create(int A[],int n)
+{
+    struct Node *t,*last;
+    int i;
+    
+    first=new struct Node;
+    first->data=A[0];
+    first->prev=first->next=NULL;
+    last=first;
+    
+    for(i=1;i<n;i++)
+    {
+        t=new struct Node;
+        t->data=A[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+
+}
+
+void Display(struct Node *p)
+{
+  while(p!=NULL)
+  {
+      cout<<p->data<<" ";
+      p=p->next;
+  }
+    
+}
+
+int Length(struct Node *p)
+{
+    int len=0;
+    while(p!=NULL)
+    {
+        len++;
+        p=p->next;
+    }
+    return len;
+    
+}
+
+void Insert(struct Node *p,int index,int x)
+{
+   struct Node *t;
+   int i;
+   
+   if(index<0 || index>Length(first))
+   return ;
+   
+   if(index==0)
+   {
+       t=new struct Node;
+       t->data=x;
+       t->prev=NULL;
+       t->next=first;
+       first->prev=t;
+       first=t;
+    }
+    
+    else
+    {
+        for(i=0;i<index-1;i++)
+        {
+            p=p->next;
+        }
+        t=new struct Node;
+        t->data=x;
+        
+        t->prev=p;
+        t->next=p->next;
+        if(p->next!=NULL)p->next->prev=t;
+        p->next=t;
+        
+        
+    }
+   
+}
+
+int main()
+{
+    int A[]={10,20,30,40,50};
+    Create(A,5);
+    Insert(first,4,60);
+    
+    
+    
+    Display(first);
+    
+    
+    
+}
